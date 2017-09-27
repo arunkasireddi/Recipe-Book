ea158bd1c0636bb3d1935e4163f0320e0dc8cf1b
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-27 16:55:09 +0100

    Working in progress for navigation feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+  onNavigate(selectedTab: string) {
+    this.loadedFeature = selectedTab;
+  }
 }
