7130c5724efbdecd26e035a66c4666c535d0869a
Arun Kumar <arun.kasireddi@gmail.com>
2017-10-17 12:34:48 +0100

    Passing data between services (shopping list and recipe)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
+import { ShoppingListService } from "./../shopping-list/shopping-list.service";
 import { Ingredient } from "./../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -20,7 +22,12 @@ export class RecipeService {
     )
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
